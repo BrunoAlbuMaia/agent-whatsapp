ace862f7543c45241c06f3bcfa196b3ca8277334
bruno.maia <bruno.maia@fariasbrito.com.br>
2026-01-15 23:51:20 -0300

    adicionando, logger no endpoint

--- src/Application/routes/agentRoute.py
+++ src/Application/routes/agentRoute.py
@@ -1,6 +1,10 @@
 
 from fastapi import APIRouter, Request, BackgroundTasks
+import logging
+# import logging
 
+logger = logging.getLogger("webhook")
+logging.basicConfig(level=logging.INFO)
 router = APIRouter()
 # dependencies = Dependencies()
 
@@ -13,8 +17,16 @@ async def post_conversation(
     print("Mensagem recebida:", payload)
     return request.json()
 
+
 @router.post("/messages-upsert")
 async def messages_upsert(request: Request):
-    payload = await request.json()
-    print(payload)
-    return payload
+    raw_body = await request.body()
+
+    logger.info("messages-upsert chamado")
+    logger.info("RAW BODY: %s", raw_body)
+
+    if not raw_body:
+        logger.warning("Body vazio")
+        return {"status": "ignored"}
+
+    return {"status": "ok"}
